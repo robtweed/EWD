738f24dbdc5e963c8c3be458aca3961b0983faee
Rob Tweed <rtweed@mgateway.com>
2012-07-27 22:41:43 +0100

    Build 931: SMART Container functionality added

--- SMART/_zewdSmart.m
+++ SMART/_zewdSmart.m
@@ -359,7 +359,8 @@ getManifest(sessid)
  m manifest=manifests(no)
  s json=$$arrayToJSON^%zewdJSON("manifest")
  f i=1:1:$l(json,",") d
- . s text(i)=$$replaceAll^%zewdAPI($p(json,",",i),"""","\""")
+ . ;s text(i)=$$replaceAll^%zewdAPI($p(json,",",i),"""","\""")
+ . s text(i)=$p(json,",",i)
  . i i'=$l(json,",") s text(i)=text(i)_","
  d setTextAreaValue^%zewdExt4Code(.text,"manifest",sessid)
  QUIT ""
